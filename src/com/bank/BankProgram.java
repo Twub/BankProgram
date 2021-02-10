99c6f801f922e652fa305bd3038bd2ee48ecf2c2
Hampus Nilsson <hampus10699@gmail.com>
2021-02-10 12:05:34 +0100

    0

--- src/com/bank/BankProgram.java
+++ src/com/bank/BankProgram.java
@@ -1,6 +1,7 @@
 package com.bank;
 
 import com.bank.Objects.BankAccount;
+import com.bank.Utility.Factory;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -8,7 +9,8 @@ import java.util.Scanner;
 public class BankProgram {
 
     private static BankProgram instance = null; // SINGLETON Can only create one instance of this class
-    private ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+    private ArrayList<BankAccount> accounts = new ArrayList<BankAccount>(); // stores all accounts.
+    private boolean isRunning = true;
 
     // private constructor for this class, so that you cant create multiple instances of this class
     // you need to get the only instance from the getInstance() method.
@@ -19,10 +21,67 @@ public class BankProgram {
     // This run method runs the whole application.
     public void run(){
         Scanner input = new Scanner(System.in);
+        while (isRunning){
+            MenuItem menuChoice = showAndGetMenuChoice(input);
+            switch (menuChoice) {
+                case SHOW_ALL_ACCOUNTS:
+                    showAllAccounts();
+                    break;
+                case SELECT_ACCOUNT:
+                    selectAccount();
+                    break;
+                case CREATE_ACCOUNT:
+                    createAccount(input);
+                    break;
+            }
+        }
+    }
+
+    private void showAllAccounts(){
+        for (int i = 0; i < accounts.size(); i++){
+            String firstName = accounts.get(i).getFirstName();
+            String lastName = accounts.get(i).getLastName();
+            System.out.println((i+1) + ": " + firstName + " " + lastName);
+        }
+    }
+
+    private void selectAccount(){
+        showAllAccounts();
+        
+    }
+
+    private void createAccount(Scanner input){
+        System.out.println("Type in first name");
+        String firstName = input.nextLine();
+        System.out.println("Type in last name");
+        String lastName = input.nextLine();
+        System.out.println("Type in accountBalance");
+        double balance = input.nextDouble();
+        BankAccount account = Factory.createBankAccount(firstName, lastName, balance);
+        accounts.add(account);
     }
 
     // prints the menu and get a menu choice in return
-    
+    private MenuItem showAndGetMenuChoice(Scanner input){
+        System.out.println("1. Show all accounts");
+        System.out.println("2. Select account");
+        System.out.println("3. Exit Program");
+
+        int menuInput = input.nextInt();
+        switch (menuInput){
+            case 1:
+                return MenuItem.SHOW_ALL_ACCOUNTS;
+            case 2:
+                return MenuItem.SELECT_ACCOUNT;
+            case 3:
+                return MenuItem.CREATE_ACCOUNT;
+            case 4:
+                return MenuItem.EXIT_PROGRAM;
+                default:
+                    System.out.println("Make a choice.");
+        }
+        return null;
+    }
 
     // get the instance of this class
     public static BankProgram getInstance() {
--- src/com/bank/MenuItem.java
+++ src/com/bank/MenuItem.java
@@ -4,6 +4,8 @@ public enum MenuItem {
 
     SHOW_ALL_ACCOUNTS("1. Show all accounts"),
     SELECT_ACCOUNT("2. Select account"),
+    CREATE_ACCOUNT("3. Create new bank account"),
+    EXIT_PROGRAM("4. Exit Program"),
     TAKE_OUT_MONEY("1. Take out money"),
     PUT_IN_MONEY("2. Put in money");
 
